89a23133df6b14c87c29efd495db5cce140ab5cb
keisukemorita8113 <68324345+keisukemorita8113@users.noreply.github.com>
2021-06-29 19:09:12 +0900

    1.upload

---
+++ NumerOn/js/home.js
@@ -0,0 +1,14 @@
+document.getElementById('start').addEventListener('click', () =>{
+    if(document.getElementById('people').value === '0'){
+
+         window.location.href = './tutrial.html';
+    
+    }else if(document.getElementById('people').value === '1'){
+
+        window.location.href = './play1.html';
+
+        
+    }
+    
+})
+
